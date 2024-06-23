fe944a30ebb352aad039f2287efbab3ea865df23
Bruno Mendes <brupmendes@icloud.com>
2024-06-23 15:20:16 -0300

    feat: add MLflow for experiment tracking

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,15 +1,18 @@
 import logging
 
 import pandas as pd
+import mlflow
 from sklearn.base import RegressorMixin
 from zenml import step
+from zenml.client import Client
 
 from typing import Tuple, Annotated
 from src.evaluation import MSE, R2, RMSE
 import numpy as np
 
+experiment_tracker = Client().active_stack.experiment_tracker
 
-@step
+@step(experiment_tracker=experiment_tracker.name)
 def evaluate_model(model: RegressorMixin,
     X_test: pd.DataFrame,
     y_test: pd.DataFrame
@@ -23,12 +26,15 @@ def evaluate_model(model: RegressorMixin,
         prediction = model.predict(X_test)
         mse_class = MSE()
         mse = mse_class.calculate_scores(y_test, prediction)
+        mlflow.log_metric("mse", mse)
 
         r2_class = R2()
         r2 = r2_class.calculate_scores(y_test, prediction)
+        mlflow.log_metric("r2", r2)
 
         rmse_class = RMSE()
         rmse = rmse_class.calculate_scores(y_test, prediction)
+        mlflow.log_metric("rmse", rmse)
 
         return r2, rmse, mse
     except Exception as e:
