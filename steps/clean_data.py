f12aeb1eb49b4f817728e50e8de185a2816dbcc6
Bruno Mendes <brupmendes@icloud.com>
2024-06-23 13:29:15 -0300

    feat: calling data cleaning and splitting module in clean_data step

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,7 +1,40 @@
 import logging
+from typing import Annotated, Tuple
+
 import pandas as pd
 from zenml import step
 
+from src.data_cleaning import DataCleaning, DataDivideStrategy, DataPreProcessStrategy
+
 @step
-def clean_data(df: pd.DataFrame) -> None:
-    pass
+def clean_data(df: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+]:
+    """
+    Cleans the data and divides it into train and test
+
+    Args:
+        df: raw data
+    Returns:
+        X_train: Training data
+        X_test: Testing data
+        y_train: Training labels
+        y_test: Testing labels
+    """
+    try:
+        process_strategy = DataPreProcessStrategy()
+        data_cleaning = DataCleaning(df, process_strategy)
+        processed_data = data_cleaning.handle_data()
+
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_strategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data cleaning completed")
+
+        return X_train, X_test, y_train, y_test
+    except Exception as e:
+        logging.error(f"Error in cleaning data {e}")
+        raise e
