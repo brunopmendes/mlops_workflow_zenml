70209d1c2648edd88281000c0ab19cbf768bd66f
Bruno Mendes <brupmendes@icloud.com>
2024-06-23 13:32:57 -0300

    feat: calling config module to set the right model for training

--- steps/model_train.py
+++ steps/model_train.py
@@ -2,6 +2,39 @@ import logging
 import pandas as pd
 from zenml import step
 
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+from .config import ModelNameConfig
+
 @step
-def train_model(df: pd.DataFrame) -> None:
-    pass
+def train_model(
+    X_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.DataFrame,
+    y_test: pd.DataFrame,
+    config: ModelNameConfig
+) -> RegressorMixin:
+    """
+    Trains the model on the ingested data
+
+    Args:
+        X_train: pd.DataFrame
+        X_test: pd.DataFrame
+        y_train: pd.DataFrame
+        y_test: pd.DataFrame
+    """
+    try:
+        model = None
+        if config.model_name == "LinearRegression":
+            model = LinearRegressionModel()
+            trained_model = model.train(X_train, y_train)
+        # if config.model_name == "RandomForestRegressor":
+            # model = RandomForestRegressionModel
+            # trained_model = model.train(X_train, y_train)
+        else: 
+            raise ValueError(f"Model {config.model_name} not supported")
+        
+        return trained_model
+    except Exception as e:
+        logging.error(f"Error in training model {e}")
+        raise e
